adbcb24033c635d92ff07b94954961c6aa767978
FrancoAlfano <franco.alfano.95@gmail.com>
2019-12-04 16:18:56 -0300

    turning tarjeta-jhipster into just a spring boot

---
+++ tarjeta/src/main/java/ar/edu/um/programacion2/tarjeta/TarjetaApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.um.programacion2.tarjeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarjetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarjetaApplication.class, args);
+	}
+
+}
