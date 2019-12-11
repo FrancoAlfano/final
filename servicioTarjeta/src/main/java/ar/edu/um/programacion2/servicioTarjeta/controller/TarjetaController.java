0c828b48f51f10a5f0d33a41aaa1f219f664acc1
FrancoAlfano <franco.alfano.95@gmail.com>
2019-12-11 17:34:39 -0300

    completed json error responses

--- servicioTarjeta/src/main/java/ar/edu/um/programacion2/servicioTarjeta/controller/TarjetaController.java
+++ servicioTarjeta/src/main/java/ar/edu/um/programacion2/servicioTarjeta/controller/TarjetaController.java
@@ -55,9 +55,23 @@ public class TarjetaController {
 		}		
 	}
 	
-	@GetMapping("/monto/{tarjetaId}/{monto}")
-	public ResponseEntity<Tarjeta> findByIdAndMonto(@PathVariable Long tarjetaId, @PathVariable Double monto){
-		return new ResponseEntity<Tarjeta>(service.findByIdAndMonto(tarjetaId, monto), HttpStatus.OK);
+	@PostMapping("/monto")
+	public ResponseEntity<Object> findByIdAndMonto(@RequestBody Tarjeta tar){
+		Long tarjetaId = tar.getId();
+		Double monto = tar.getMonto();
+		JSONObject jo = new JSONObject();
+		
+		tar = service.findById(tarjetaId);
+		
+		if (tar != null && monto<=tar.getMonto()) {
+			return new ResponseEntity<Object>(HttpStatus.CREATED);
+			
+		} else {
+			
+			jo.put("codError", "30");
+			jo.put("error", "Monto máximo de venta superado");
+			return new ResponseEntity<Object>(jo.toString(), HttpStatus.FORBIDDEN);
+		}
 		
 	}
 	
