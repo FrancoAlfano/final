8d7fae18e71bb5ac182d6506dae4b149a08357d9
FrancoAlfano <franco.alfano.95@gmail.com>
2020-01-29 18:42:10 -0300

    testing

--- servicioTarjeta/src/main/java/ar/edu/um/programacion2/servicioTarjeta/repository/ITarjetaRepository.java
+++ servicioTarjeta/src/main/java/ar/edu/um/programacion2/servicioTarjeta/repository/ITarjetaRepository.java
@@ -2,6 +2,8 @@ package ar.edu.um.programacion2.servicioTarjeta.repository;
 
 
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import ar.edu.um.programacion2.servicioTarjeta.model.Tarjeta;
@@ -11,5 +13,7 @@ import ar.edu.um.programacion2.servicioTarjeta.model.Tarjeta;
 public interface ITarjetaRepository extends JpaRepository<Tarjeta, Long> {
 
 	Tarjeta findByIdAndMonto(Long tarjetaId, Double monto);
+
+	Optional<Tarjeta> findByIdAndClienteId(Long tarjeta_id, Long cliente_id);
 
 }
