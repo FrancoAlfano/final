aae3add558f932725bf7ba6dbcef4203b526d9af
FrancoAlfano <franco.alfano.95@gmail.com>
2020-02-01 16:10:22 -0300

    removing unused imports

--- servicioVentas/src/main/java/ar/edu/um/programacion2/servicioVentas/controller/VentaController.java
+++ servicioVentas/src/main/java/ar/edu/um/programacion2/servicioVentas/controller/VentaController.java
@@ -1,23 +1,17 @@
 package ar.edu.um.programacion2.servicioVentas.controller;
 
 import java.util.List;
-import java.util.Optional;
 
-import org.apache.catalina.startup.ClassLoaderFactory.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 import ar.edu.um.programacion2.servicioVentas.model.Venta;
 import ar.edu.um.programacion2.servicioVentas.service.VentaService;
 
--- servicioVentas/src/main/java/ar/edu/um/programacion2/servicioVentas/model/Venta.java
+++ servicioVentas/src/main/java/ar/edu/um/programacion2/servicioVentas/model/Venta.java
@@ -1,8 +1,6 @@
 package ar.edu.um.programacion2.servicioVentas.model;
 
 import java.io.Serializable;
-import java.util.Date;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
